b7dbdfa4d2fb8929b43b9bd341715ee8d42ac46f
Daxrokz10 <dakshgagnani@gmail.com>
2025-12-03 20:53:42 +0530

    unit 4

---
+++ searching_sorting/sorting/mergeSort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+
+    int size = right - left + 1;
+    int temp[size];
+
+    while (i <= mid && j <= right)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= right)
+    {
+        temp[k++] = arr[j++];
+    }
+
+    for (i = 0, k = 0; i < size; i++, k++)
+    {
+        arr[left + i] = temp[k];
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main()
+{
+    int arr[50], n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array with merge sort: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
