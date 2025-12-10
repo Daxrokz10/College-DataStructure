2e924bceac3233008af848b9d0cf68ea9a7d7b72
Daxrokz10 <dakshgagnani@gmail.com>
2025-12-10 15:10:00 +0530

    qs

--- self_sort/quickSort.c
+++ self_sort/quickSort.c
@@ -8,11 +8,24 @@ void swap(int *a,int *b){
 }
 
 int partition(int arr[],int low,int high){
-    
+    int pivot = arr[high];
+    int i = low -1;
+    for(int j = low;j<high;j++){
+        if(arr[j]<pivot){
+            i++;
+            swap(&arr[i],&arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return i+1;
 }
 
 void quickSort(int arr[],int low,int high){
-
+    if(low<high){
+        int pi = partition(arr,low,high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
+    }
 }
 
 int main()
@@ -26,7 +39,7 @@ int main()
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
     
-
+    quickSort(arr,0,n-1);
 
     printf("Sorted array with quick sort: ");
     for(int i=0;i<n;i++){
